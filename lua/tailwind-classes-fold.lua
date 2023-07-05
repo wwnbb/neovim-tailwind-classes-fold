6e64cedae46982f754310cd98c19fad8559f5b89
wwnbb <wwnbb1@gmail.com>
2023-07-05 20:33:04 +0400

    update

--- lua/tailwind-classes-fold.lua
+++ lua/tailwind-classes-fold.lua
@@ -1,5 +1,16 @@
+local module = require("tailwind-classes-fold.module")
+local autocmd = require("tailwind-classes-fold.autocmd")  -- Require new module
+
 local M = {}
+M.config = {
+  -- default config
+  opt = "Hello!",
+}
 
--- Add your functions here
+M.setup = function(args)
+  -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
+  -- you can also put some validation here for those.
+  M.config = vim.tbl_deep_extend("force", M.config, args or {})
 
-return M
+  autocmd.setup_autocmd()  -- Setup autocmds
+end
