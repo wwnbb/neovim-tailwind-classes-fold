742cb054a98e4ff2731eec825c84ef4aedaf91e9
wwnbb <wwnbb1@gmail.com>
2023-07-05 21:33:54 +0400

    update plugin

---
+++ lua/tailwind-classes-fold/init.lua
@@ -0,0 +1,14 @@
+local M = {}
+local autocmd = require("tailwind-classes-fold.autocmd")
+
+
+M.setup = function()
+  autocmd.setup_autocmd()
+end
+
+M.toggle_conceal = function()
+  local currConcealLevel = vim.wo.conceallevel
+  vim.wo.conceallevel = (currConcealLevel > 0) and 0 or 2
+end
+
+return M
